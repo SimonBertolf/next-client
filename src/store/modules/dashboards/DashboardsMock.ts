0587809683b03288abdc0e1771cc4ed2759110af
Timon Heinis <timon@embrio.tech>
2021-06-14 18:06:52 +0200

    more mock dashboards

--- src/store/modules/dashboards/DashboardsMock.ts
+++ src/store/modules/dashboards/DashboardsMock.ts
@@ -2,11 +2,27 @@ import { Dashboard } from '@/types';
 
 export const dashboardsMock: Dashboard[] = [
   {
-    name: 'Overview',
+    name: 'Übersicht',
     _id: '60c717880e671f91b56bce2d',
   },
   {
-    name: 'Financial',
+    name: 'Finanzen',
     _id: '60c717a21b5d98c49fc4e8fe',
+  },
+  {
+    name: 'Gebäudefarben',
+    _id: '60c77e01e9481dd72235af88',
+  },
+  {
+    name: 'Lage',
+    _id: '60c77e180a0a2b9da8a02a53',
+  },
+  {
+    name: 'Stockwerke',
+    _id: '60c77e1efbde78f162d9c70c',
+  },
+  {
+    name: 'Nutzung',
+    _id: '60c77e2486a17d53d5f95ab9',
   },
 ];
