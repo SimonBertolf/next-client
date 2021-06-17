e8c22a117a3ddfc9a3caf29c453077e09cf2a52c
Timon Heinis <timon@embrio.tech>
2021-06-17 15:00:29 +0200

    use realistic asset ids in mock

--- __mocks__/AssetMocks.ts
+++ __mocks__/AssetMocks.ts
@@ -1,18 +1,18 @@
 import { Asset } from '../src/models';
 
 export const assetMock: Asset = {
-  id: '60c9cbd35f6db188c6441116',
-  name: 'Lindenbachstrasse 40, 8006 Zürich',
+  id: '9',
+  name: 'Damstrasse 7, 8006 Zürich',
 };
 
 export const assetsMock: Asset[] = [
   assetMock,
   {
-    id: '60c9cbdd5d40b57b5c5cd5b1',
+    id: '10',
     name: 'Forststrasse 23, 8006 Zürich',
   },
   {
-    id: '60c9cbe2b62587d676cf5c1e',
+    id: '11',
     name: 'Giesshübelstrasse 84, 8045 Zürich',
   },
 ];
