e766d6d8d663f81f6f5486bd267042cb12c5d392
Timon Heinis <timon@embrio.tech>
2021-07-13 19:13:35 +0200

    fix lint errors in TypeDocMain

--- TypeDocMain.ts
+++ TypeDocMain.ts
@@ -1,33 +1,34 @@
-const TypeDoc = require("typedoc");
+// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
+const TypeDoc = require('typedoc');
 
 class TypeDocMain {
-    async main() {
-        const app = new TypeDoc.Application();
-        // If you want TypeDoc to load tsconfig.json / typedoc.json files
-        app.options.addReader(new TypeDoc.TSConfigReader());
-        app.options.addReader(new TypeDoc.TypeDocReader());
+  async main() {
+    const app = new TypeDoc.Application();
+    // If you want TypeDoc to load tsconfig.json / typedoc.json files
+    app.options.addReader(new TypeDoc.TSConfigReader());
+    app.options.addReader(new TypeDoc.TypeDocReader());
 
-        app.bootstrap({
-            // typedoc options here
-            entryPoints: [
-                "src/services/index.ts",
-                "src/inversify/index.ts",
-                "src/types/index.ts",
-                "src/models/index.ts",
-                "src/store/modules/index.ts"
-            ],
-        });
+    app.bootstrap({
+      // typedoc options here
+      entryPoints: [
+        'src/services/index.ts',
+        'src/inversify/index.ts',
+        'src/types/index.ts',
+        'src/models/index.ts',
+        'src/store/modules/index.ts',
+      ],
+    });
 
-        const project = app.convert();
+    const project = app.convert();
 
-        if (project) {
-            // Project may not have converted correctly
-            const outputDir = "docs";
+    if (project) {
+      // Project may not have converted correctly
+      const outputDir = 'docs';
 
-            // Rendered docs
-            await app.generateDocs(project, outputDir);
-        }
+      // Rendered docs
+      await app.generateDocs(project, outputDir);
     }
+  }
 }
 
 module.exports = TypeDocMain;
