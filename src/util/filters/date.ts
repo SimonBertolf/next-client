839886c2c342aa2aad282c0becde1d57f185d2ee
Dragan Gojic <dragan.gojic@reamis.ch>
2021-08-03 11:10:08 +0200

    error handling dateFormatFilter

--- src/util/filters/date.ts
+++ src/util/filters/date.ts
@@ -6,6 +6,6 @@ export const dateTimeFilter = (value: string): string => {
 export const dateFormatFilter = (value: string): string => {
   if (value === '0000-00-00') return '-';
   const itsDate = new Date(value);
-  if (!itsDate) throw new Error(`invalid date format for parameter value, with value: ${value}`);
+  if (Number.isNaN(itsDate.getTime())) throw new Error(`invalid date format for parameter value, with value: ${value}`);
   return `${itsDate.getDay()}.${itsDate.getMonth() + 1}.${itsDate.getFullYear()}`;
 };
