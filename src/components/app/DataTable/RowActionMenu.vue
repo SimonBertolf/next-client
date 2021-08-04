867d460d5cef26cebc6cb3ed09fa2e4b1240c452
Dragan Gojic <dragan.gojic@reamis.ch>
2021-08-04 12:46:11 +0200

    create row-action-menu component

---
+++ src/components/app/DataTable/RowActionMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <a-dropdown :trigger="['click']" class="row-action-menu">
+    <a-icon type="more" :rotate="90" />
+    <a-menu slot="overlay" @click="click">
+      <a-menu-item v-for="{ key, label } in options" :key="key">{{ label }}</a-menu-item>
+    </a-menu>
+  </a-dropdown>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Emit } from 'vue-property-decorator';
+
+@Component
+export default class RowActionMenu extends Vue {
+  @Prop({ type: String, required: true }) rowKey: string;
+
+  @Prop({ type: Array, default: [] }) options: Array<{ key: string; label: string }>;
+
+  @Emit()
+  click({ key }: { key: string }) {
+    return { key, rowKey: this.rowKey };
+  }
+}
+</script>
+
+<style scoped>
+.row-action-menu {
+  cursor: pointer;
+}
+</style>
