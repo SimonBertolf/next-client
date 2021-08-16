f0de9a7d5341f429dde4c3ddcfe64f23a111eb86
Simon Bertolf <simon.bertolf@reamis.ch>
2021-08-16 13:46:41 +0200

    WidgetTitle and WidgetText

--- src/components/app/WidgetLayout/Widget/index.ts
+++ src/components/app/WidgetLayout/Widget/index.ts
@@ -1,6 +1,6 @@
 import Widget from './Widget.vue';
 import AddWidget from './AddWidget.vue';
-import WidgetTitle from './WidgetTitle.vue';
-import WidgetText from './WidgetText.vue';
 
-export { Widget, AddWidget, WidgetTitle, WidgetText };
+export { Widget, AddWidget };
+export * from './WidgetTitle';
+export * from './WidgetText';
