0aecff3e8d72ae26becc43b220098c0f416ffd40
Simon Bertolf <simon.bertolf@reamis.ch>
2021-08-18 09:07:54 +0200

    WidgetTitle and Colortype

--- src/components/data-visualization/WidgetTitle/WidgetTitle.vue
+++ src/components/data-visualization/WidgetTitle/WidgetTitle.vue
@@ -1,9 +1,10 @@
 <template>
-  <h2 :class="cls">{{ title }}</h2>
+  <h3 :class="cls">{{ title }}</h3>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
+import { ColorType } from '@/types/ColorType';
 
 @Component({ components: {} })
 export default class WidgetTitle extends Vue {
@@ -11,10 +12,11 @@ export default class WidgetTitle extends Vue {
 
   @Prop({ default: 'left', type: String }) readonly align: string;
 
-  @Prop({ default: 'dark', type: String }) readonly color: string;
+  @Prop() readonly color: ColorType | undefined;
 
   get cls() {
-    return `text-3xl font-bold text-${this.color} text-${this.align} truncate`;
+    if (this.color) return `text-3xl font-bold text-${this.color} text-${this.align} truncate`;
+    return `text-3xl font-bold text-${this.align} truncate`;
   }
 }
 </script>
---
+++ src/types/ColorType/ColorType.ts
@@ -0,0 +1,5 @@
+export interface ColorType {
+  primary: string;
+  primaryl: string;
+  secondary: string;
+}
