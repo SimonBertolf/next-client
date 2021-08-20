628191c7b0d3b49638b8db4a3f7f9c20b7db5fe5
Simon Bertolf <simon.bertolf@reamis.ch>
2021-08-20 15:12:19 +0200

    ChartType

--- src/types/Chart.ts
+++ src/types/Chart.ts
@@ -2,28 +2,23 @@ export interface ChartData {
   [key: string]: number | string;
 }
 
-export interface XYChartSeries {
+interface XYChartSeries {
   x: { key: string };
   y: { key: string };
-  z: { key: string; showValue: boolean; valueFillColor?: string; valueTextColor?: string; unit?: string };
+  color?: string;
 }
 
-export interface LineChartSeries {
-  x: { key: string };
-  y: { key: string };
-  showStroke: boolean;
-  showBullet: boolean;
-  strokeColor?: string;
-  bulletFillCollor?: string;
+export interface LineChartSeries extends XYChartSeries {
+  showStroke?: boolean; // default true
+  showBullet?: boolean; // default flse
+}
+
+export interface BubbelChartSeries extends XYChartSeries {
+  z: { key: string; showValue?: boolean; unit?: string };
+  textColor?: string;
 }
 
 export interface XYChartAxes {
-  x: { label: string; unit?: string };
-  y: { label: string; unit?: string };
-  z: { label: string; unit?: string };
-}
-
-export interface LineChartAxes {
   x: { label: string; unit?: string };
   y: { label: string; unit?: string };
 }
